c8b8a78f3a7ecc89a1eeb8903e3f4a1c338d20d5
Markus Klein <m@mklein.co.at>
2016-03-25 00:41:27 +0100

    move home view route; define auth routes manually

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
