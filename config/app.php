43018184ed61920df1e50589a7638d32f9cb8ea0
Markus Klein <m@mklein.co.at>
2016-03-28 23:07:14 +0200

    add laravel helper for phpstorm support

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
