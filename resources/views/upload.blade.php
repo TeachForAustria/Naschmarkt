b61e75f0349de3433e49c7ee5fe4fffcb636a00d
Markus Klein <m@mklein.co.at>
2016-04-17 22:38:22 +0200

    fix upload page

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -18,89 +18,58 @@
                     <!-- Header -->
                     <div class="panel-heading">Upload</div>
                     <!-- Body -->
-                    <div class="panel-body">
-                        <div class="form-horizontal">
-
-                            <!-- title -->
+                    <div class="panel-body col-sm-offset-1">
+                        <form action="/upload" method="POST" enctype="multipart/form-data" class="form-horizontal">
+                            {!! csrf_field() !!}
+                            <!-- name -->
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Titel*</label>
+                                <label class="col-md-2 control-label">Name*</label>
 
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" name="title">
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="title" />
                                 </div>
                             </div>
 
-                            <!-- file
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label">Datei*</label>
-
-                                    <div class="input-group">
-                                        <form action="/upload" method="POST" enctype="multipart/form-data">
-                                            {!! csrf_field() !!}
-                                    <span class="input-group-btn">
-                                        <span class="btn btn-default btn-file">
-                                            Browse&hellip; <input name="file" type="file" class="form-control"/>
-                                                </span>
-                                            </span>
-                                            <input type="text" class="form-control" disabled>
-
-                                            <br/><br/>
-                                            <button type="submit" class="btn btn-primary">Upload</button>
-                                        </form>
-                                    </div> -->
-
-
                             <!-- description -->
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Beschreibung </label>
-                                <div class="col-md-6">
+                                <label class="col-md-2 control-label">Beschreibung</label>
+                                <div class="col-md-8">
                                     <textarea class="form-control" rows="4" id="description"></textarea>
                                 </div>
                             </div>
 
                             <!-- tags -->
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Tags*</label>
-                                <div class="col-md-6">
-                                    <input type="text" value="Wird, Noch ,Ge&auml;ndert" class="form-control" name="title">
+                                <label class="col-md-2 control-label">Tags*</label>
+                                <div class="col-md-8">
+                                    <input type="text" value="Wird, Noch ,Ge&auml;ndert" class="form-control" name="title" />
                                     <span class="help-block">
                                         Mindestens 3 Tags, um die Suche zu erleichtern.
                                     </span>
                                 </div>
                             </div>
 
-                            <!-- <input type="text" value="Test,Hallo"  data-role="tagsinput" /> -->
-
                             <!-- file -->
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Datei*</label>
-                                <div class="col-md-6">
-                                    <form action="/upload" method="POST" enctype="multipart/form-data"
-                                          style="float: left;">
-                                        <div class="input-group">
-                                            {!! csrf_field() !!}
-                                            <span class="input-group-btn">
-                                                    <span class="btn btn-primary btn-file">
-                                                        Browse&hellip; <input name="file" type="file"
-                                                                              class="form-control" id="browseButton"/>
-                                                    </span>
-                                                </span>
-                                            <input type="text" class="form-control" id="fileName" disabled>
-                                        </div>
-                                        <br/>
-                                        <button type="submit" class="btn btn-primary">Upload</button>
-                                    </form>
+                                <label class="col-md-2 control-label">Datei*</label>
+                                <div class="col-md-8">
+                                    <input name="file" type="file" id="invisibleButton" />
                                 </div>
-                            </div> <!-- /form-group -->
+                            </div>
 
 
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button type="submit" class="btn btn-default">Hochladen</button>
+                                </div>
+                            </div>
 
                             <!-- help-block -->
                             <br/><br/>
                             <span class="help-block">
                                 *Felder m&uuml;ssen ausgef&uuml;llt werden.
                             </span>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
