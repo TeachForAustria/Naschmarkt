28d591990ca39700baa1deda7216cbe10781baf9
Philip Vonderlind <pvonderlin@student.tgm.ac.at>
2016-04-27 09:55:45 +0000

    Changed stuff

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -16,28 +16,28 @@
 @section('content')
 
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Search</div>
-                    <div class="panel-body">
-                        <form action="/search" method="POST" class="form-horizontal" enctype="multipart/form-data">
-                            {!! csrf_field() !!}
-                            <div class="form-group">
-                                <div class="col-md-10">
-                                    <input id="searchQuery" type="text"  data-role="tagsinput" name="searchQuery" class="form-control">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Search</div>
+                        <div class="panel-body">
+                            <form action="/search" method="POST" class="form-horizontal" enctype="multipart/form-data">
+                                {!! csrf_field() !!}
+                                <div class="form-group">
+                                    <div class="col-md-10">
+                                        <input id="searchQuery" type="text"  data-role="tagsinput" name="searchQuery" class="form-control">
+                                    </div>
+                                    <div class="col-md-2">
+                                        <button type="submit" class="btn btn-default" name="search">Search</button>
+                                    </div>
                                 </div>
-                                <div class="col-md-2">
-                                    <button type="submit" class="btn btn-default" name="search">Search</button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
 <!-- AJAX SHIT
     <div class="container">
