c846ee4cf05363ba8bb0f234e256f3c74c49851e
tfellner-tgm <tfellner@student.tgm.ac.at>
2016-05-17 16:30:22 +0200

    finish migration script

--- migrationscript/migrateFromOldToNew.py
+++ migrationscript/migrateFromOldToNew.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python
 import cgi
+import os
 import urllib2
 import uuid
 from ftplib import FTP
-
 import MySQLdb
 import re
+import time
 
+start_time = time.time()
 TAG_RE = re.compile(r'<[^>]+>')
+
 
 def remove_tags(text):
     return TAG_RE.sub('', text)
 
+
 dbOld = MySQLdb.connect(
-    host="mysql003.tophosting.at",      # host ip
-    user="dbue88a3f4",                  # username
-    passwd="Khy?8k9p",                  # password
-    db="dbf26d5267"                     # name of the database
+    host="mysql003.tophosting.at",  # host ip
+    user="dbue88a3f4",  # username
+    passwd="Khy?8k9p",  # password
+    db="dbf26d5267"  # name of the database
 )
 
 dbNew = MySQLdb.connect(
-    host="127.0.0.1",                   # host ip
-    port=33060,                         # port
-    user="homestead",                   # username
-    passwd="secret",                    # password
-    db="homestead"                      # name of the database
+    host="127.0.0.1",  # host ip
+    port=33060,  # port
+    user="homestead",  # username
+    passwd="secret",  # password
+    db="homestead"  # name of the database
 )
 
 # Create a Cursor object
@@ -39,7 +43,7 @@ insertCursor = dbNew.cursor()
 
 # 
 for row in cur.fetchall():
-    id = row[0]
+    old_post_id = row[0]
     title = row[1]
     description = remove_tags(row[2])
     date = row[3]
@@ -62,11 +66,14 @@ for row in cur.fetchall():
 
         cur.execute(
             "SELECT term.name FROM (wp_terms AS term INNER JOIN wp_term_taxonomy AS tax ON term.term_id = tax.term_id) INNER JOIN wp_term_relationships AS relation ON relation.term_taxonomy_id = tax.term_taxonomy_id WHERE relation.object_id = {0}".format(
-                id))
+                old_post_id))
 
         print "    TAGS"
         for row in cur.fetchall():
             tag = row[0]
+
+            if tag[0] is '_':
+                tag = tag[1:]
 
             print "    " + tag
 
@@ -74,14 +81,14 @@ for row in cur.fetchall():
                 "SELECT EXISTS(SELECT * FROM tags where value = '{0}')".format(
                     tag))
 
-
             if insertCursor.fetchall()[0][0] == 1:
                 insertCursor.execute(
                     "SELECT id FROM tags WHERE value = '{0}'".format(
                         tag))
                 tag_id = insertCursor.fetchall()[0][0]
 
-                insertCursor.execute("INSERT IGNORE INTO post_tag (post_id, tag_id) VALUES ('{0}', '{1}')".format(post_id, tag_id))
+                insertCursor.execute(
+                    "INSERT IGNORE INTO post_tag (post_id, tag_id) VALUES ('{0}', '{1}')".format(post_id, tag_id))
             else:
                 try:
                     insertCursor.execute(
@@ -93,16 +100,22 @@ for row in cur.fetchall():
                         "INSERT INTO post_tag (post_id, tag_id) VALUES ('{0}', '{1}')".format(
                             post_id, tag_id))
                 except:
-                    print "Error inserting tags into the Database"
+                    print 'Error inserting tags into the Database'
                     dbNew.rollback()
 
-
-
-
     # Use all the SQL you like
-    cur.execute("SELECT guid FROM wp_posts WHERE post_type=\"attachment\" && guid != ' ' && post_parent = {0}".format(id))
+    cur.execute(
+        "SELECT guid FROM wp_posts WHERE post_type=\"attachment\" && guid != ' ' && post_parent = {0}".format(old_post_id))
 
+    # File Document downloading starts here
     files = []
+
+    ftp = FTP('192.168.10.10')
+    ftp.login('vagrant', 'vagrant')
+
+    ftp.cwd('naschmarkt')
+    ftp.cwd('storage')
+    ftp.cwd('app')
 
     print "    ATTACHMENT URLS"
     # print all the first cell of all the rows
@@ -110,32 +123,25 @@ for row in cur.fetchall():
         try:
             attachmentURL = row[0]
 
-            print "    " + attachmentURL
-
             filename = uuid.uuid4()
 
-
-            ftp = FTP('192.168.10.10')
-            ftp.login('vagrant', 'vagrant')
-
-            ftp.cwd('naschmarkt')
-            ftp.cwd('storage')
-            ftp.cwd('app')
+            if '?attachment_id=' in attachmentURL:
+                attachmentURL = attachmentURL.replace('attachment_id', 'download')
 
             response = urllib2.urlopen(attachmentURL)
+
+            print "    " + attachmentURL + " -> " + response.geturl()
 
             originalFilename = urllib2.unquote(response.geturl()).split('/')[-1]
 
             extension = originalFilename.split('.')[-1]
 
-            downloaded_file = open("tmp", "w")
-            downloaded_file.write(response.read())
-            downloaded_file.close()
+            file_to_upload = open('tmp_file_to_upload', 'w')
+            file_to_upload.write(response.read())
+            file_to_upload.close()
 
-            with open('tmp', 'rb') as ftpup:
+            with open('tmp_file_to_upload', 'rb') as ftpup:
                 ftp.storbinary('STOR ' + str(filename) + '.' + extension, ftpup)
-
-            ftp.quit()
 
             try:
                 insertCursor.execute(
@@ -143,7 +149,7 @@ for row in cur.fetchall():
                         post_id, originalFilename, date))
                 dbNew.commit()
             except:
-                print "Error inserting documents into the Database"
+                print '    Error inserting documents into the Database'
                 dbNew.rollback()
 
             document_id = insertCursor.lastrowid
@@ -154,12 +160,16 @@ for row in cur.fetchall():
                         document_id, date, str(filename), extension))
                 dbNew.commit()
             except:
-                print "Error inserting document versions into the Database"
+                print '    Error inserting document versions into the Database'
                 dbNew.rollback()
-        except urllib2.HTTPError:
-            print 'Could not download file'
 
+        except urllib2.HTTPError, err:
+            print '    ' + str(err.code) + ' file not found'
 
+    ftp.quit()
 
+os.remove('tmp_file_to_upload')
 dbOld.close()
 dbNew.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
