d2da8cd6ad9ec4d35c538068d5f3f8d258df485b
Markus Klein <m@mklein.co.at>
2016-05-28 22:31:46 +0200

    adapt test cases to new upload system

--- tests/DocumentVersionTest.php
+++ tests/DocumentVersionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
+
 class ConcreteDocumentTest extends TestCase
 {
     public function testWriteContent()
@@ -12,7 +14,8 @@ class ConcreteDocumentTest extends TestCase
         $this->assertEquals('foobar', Storage::get($documentVersion->uuid . '.' . $documentVersion->extension));
     }
 
-    public function testReadContent() {
+    public function testReadContent()
+    {
         $documentVersion = new \App\DocumentVersion();
         $documentVersion->generateUuid();
         $documentVersion->extension = 'txt';
@@ -20,4 +23,31 @@ class ConcreteDocumentTest extends TestCase
 
         $this->assertEquals('foobar', $documentVersion->readContent());
     }
+
+    public function testFileUpload()
+    {
+
+        $this->withoutMiddleware();
+        $uploadedFile  = self::getMockFile('storage/test_files/file_upload.txt');
+        $response = $this->call(
+            'POST',
+            '/document-versions',
+            [],
+            [],
+            ['file' => $uploadedFile]
+        );
+
+        $this->seeInDatabase('document_versions', [
+            'uuid' => json_decode($response->content(), true)['uuid']
+        ]);
+    }
+
+    public static function getMockFile($path)
+    {
+        TestCase::assertFileExists($path);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $path);
+        return new \Symfony\Component\HttpFoundation\File\UploadedFile ($path, null, $mime, null, null, true);
+    }
+
 }
