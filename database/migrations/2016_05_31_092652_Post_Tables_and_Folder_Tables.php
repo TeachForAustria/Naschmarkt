4542b3602af1bc76bff987e073915b03fb4566a6
Philip Vonderlind <pvonderlin@student.tgm.ac.at>
2016-05-31 10:18:32 +0000

    added migration

--- database/migrations/2016_05_31_092652_Post_Tables_and_Folder_Tables.php
+++ database/migrations/2016_05_31_092652_Post_Tables_and_Folder_Tables.php
@@ -12,7 +12,25 @@ class PostTablesAndFolderTables extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('projects', function ($table){
+            $table->increments('id');
+            $table->integer('owner_id');
+            $table->string('name', 200);
+            $table->timestamps();
+        });
+
+        Schema::create('folders', function ($table){
+            $table->increments('id');
+            $table->string('name', 200);
+            $table->integer('project_id');
+            $table->timestamps();
+        });
+
+        Schema::create('folder_post', function ($table){
+            $table->integer('folder_id');
+            $table->integer('post_id');
+            $table->primary(['folder_id', 'post_id']);
+        });
     }
 
     /**
