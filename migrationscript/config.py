e75ff3c7ec8e23e60b3daf33dc238e98cd7172ea
Markus Klein <m@mklein.co.at>
2016-06-07 23:26:31 +0200

    add config template for migration script

---
+++ migrationscript/config.py
@@ -0,0 +1,35 @@
+"""
+This is the config file for the Migration
+There are 3 things to configure.
+    - the old Database to migrate from
+    - the new Database to save the migration
+    - FTP connection to save the files
+"""
+
+# Old Database.
+# This is where the Data is taken from
+dbOld = {
+    'host':         "",   # host ip
+    'port':         0,                       # port
+    'user':         "",               # username
+    'password':     "",                 # password
+    'database':     ""                # name of the database
+}
+
+# New Database.
+# This is where the Data will be stored
+dbNew = {
+    'host':         "",                # host ip
+    'port':         0,                      # port
+    'user':         "",                # username
+    'password':     "",                   # password
+    'database':     ""                 # name of the database
+}
+
+# FTP connection to save the files
+ftpConnection = {
+    'host':         "",                 # host ip
+    'user':         "",                  # username
+    'password':     "",                  # password
+    'directory':    ""                  # directory where to save the files to
+}
