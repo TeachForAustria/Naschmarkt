b982480e7ed4c270a1192593d3309261650a19b0
Markus Klein <m@mklein.co.at>
2016-06-12 01:56:53 +0200

    add footer and update readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Naschmarkt :rocket: :cake: :fireworks:
-
 [![Build Status](https://travis-ci.org/TeachForAustria/Naschmarkt.svg?branch=master)](https://travis-ci.org/TeachForAustria/Naschmarkt)
+
+[Naschmarkt](http://der-naschmarkt.at) is the filesharing platform of [Teach for Austria](http://www.teachforaustria.at/).
+
+### Developers
+- [Markus Klein](https://github.com/kleiinnn/)
+- [Thomas Fellner](https://github.com/tfellner-tgm/)
+- [Philip Vonderlind](https://github.com/pvonderlin-tgm/)
+- [Matthias Schwebler](https://github.com/mschwebler-tgm/)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,105 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Naschmarkt</title>
+        <title>Naschmarkt</title>
 
-    <!-- Styles -->
-    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('lib/lato-font/css/lato.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-    @stack('stylesheets')
+        <!-- Styles -->
+        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('lib/lato-font/css/lato.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+        @stack('stylesheets')
 
-</head>
-<body id="app-layout">
+    </head>
+    <body id="app-layout">
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-<nav class="navbar navbar-inverse navbar-static-top">
-    <div class="container">
-        <div class="navbar-header">
+        <div class="wrapper">
+            <nav class="navbar navbar-inverse navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
 
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+                        <!-- Collapsed Hamburger -->
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
 
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
-                <span class="" aria-hidden="true"></span>
-                Naschmarkt
-            </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/about') }}">About</a></li>
-
-                @if(!isset($posts) && Auth::check())
-                    <form action="{{ url('/posts') }}" method="GET" class="navbar-form navbar-left" role="search" enctype="multipart/form-data">
-                        {!! csrf_field() !!}
-                        <div class="form-group">
-                            <input data-hotkey="q" name="q" placeholder="Search Posts" aria-label="Search Posts" data-unscoped-placeholder="Search Posts" data-scoped-placeholder="Search" tabindex="1" type="text" class="form-control header-search-input js-site-search-focus">
-                        </div>
-                    </form>
-                @endif
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::check())
-                    <li><a href="{{ url('/posts') }}"> Posts </a></li>
-                    <li><a href="{{ url('/project') }}"> Project </a></li>
-                    <li><a href="{{ url('/cloud') }}"> Wordcloud </a></li>
-                    <li><a href="{{ url('/upload') }}">Upload</a></li>
-                    @can('register-user')
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @endcan
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                        <!-- Branding Image -->
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <span><img src="{{ URL::asset('img/vogel_white.png') }}"></span>
+                            Naschmarkt
                         </a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/user') }}"><i class="fa fa-btn"></i>Profile</a></li>
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
+                    </div>
 
-<div class="main-wrapper">
-    @if(Session::has('status'))
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="alert alert-{{ session('status')['type'] }} alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        {{ session('status')['content'] }}
+                    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="nav navbar-nav">
+                            <li><a href="{{ url('/about') }}">About</a></li>
+
+                            @if(!isset($posts) && Auth::check())
+                                <form action="{{ url('/posts') }}" method="GET" class="navbar-form navbar-left" role="search" enctype="multipart/form-data">
+                                    {!! csrf_field() !!}
+                                    <div class="form-group">
+                                        <input data-hotkey="q" name="q" placeholder="Search Posts" aria-label="Search Posts" data-unscoped-placeholder="Search Posts" data-scoped-placeholder="Search" tabindex="1" type="text" class="form-control header-search-input js-site-search-focus">
+                                    </div>
+                                </form>
+                            @endif
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <!-- Authentication Links -->
+                            @if (Auth::check())
+                                <li><a href="{{ url('/posts') }}"> Posts </a></li>
+                                <li><a href="{{ url('/project') }}"> Project </a></li>
+                                <li><a href="{{ url('/cloud') }}"> Wordcloud </a></li>
+                                <li><a href="{{ url('/upload') }}">Upload</a></li>
+                                @can('register-user')
+                                <li><a href="{{ url('/register') }}">Register</a></li>
+                                @endcan
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="{{ url('/user') }}"><i class="fa fa-btn"></i>Profile</a></li>
+                                        <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                    </ul>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="main-wrapper">
+                @if(Session::has('status'))
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-10 col-sm-offset-1">
+                                <div class="alert alert-{{ session('status')['type'] }} alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    {{ session('status')['content'] }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+                @yield('content')
+            </div>
+
+            <div class="footer-push"></div>
+        </div>
+
+        <footer>
+            <div class="container footer-content">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <a href="http://www.teachforaustria.at/"><img src="{{ URL::asset('img/logo.svg') }}" class="brand"></a>
+                    </div>
+                    <div class="col-sm-4 col-sm-offset-2 pull-right footer-right">
+                        <div class="row">
+                            <a href="https://github.com/TeachForAustria/Naschmarkt/">
+                                <i class="fa fa-code" aria-hidden="true"></i> with <i class="fa fa-heart" aria-hidden="true"></i> as Open Source
+                            </a>
+                        </div>
+                        <div class="row">
+                            <i class="fa fa-copyright" aria-hidden="true"></i> 2016, Teach for Austria
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @endif
+        </footer>
 
-    @yield('content')
-</div>
-
-<!-- JavaScripts -->
-<script src="{{ URL::asset('lib/jquery/jquery.min.js') }}" ></script>
-<script src="{{ URL::asset('lib/bootstrap/bootstrap.min.js') }}" ></script>
-@stack('scripts')
-{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
+        <!-- JavaScripts -->
+        <script src="{{ URL::asset('lib/jquery/jquery.min.js') }}" ></script>
+        <script src="{{ URL::asset('lib/bootstrap/bootstrap.min.js') }}" ></script>
+        @stack('scripts')
+        {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    </body>
 </html>
