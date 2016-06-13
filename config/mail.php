759f27c7efed037648bafc73d9189c1a21b622b3
tfellner-tgm <tfellner@student.tgm.ac.at>
2016-06-13 16:38:18 +0200

    add mail configuration and move app_secrets and ids to .env

--- config/mail.php
+++ config/mail.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', ''),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@der-naschmarkt.at', 'name' => 'no-reply'],
 
     /*
     |--------------------------------------------------------------------------
