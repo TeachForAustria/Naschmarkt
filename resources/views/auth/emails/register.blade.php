05b1f06002f7e41b590098588072a2d23bc25717
Markus Klein <m@mklein.co.at>
2016-06-13 14:59:49 +0200

    fix activation email formatting

--- resources/views/auth/emails/register.blade.php
+++ resources/views/auth/emails/register.blade.php
@@ -1,6 +1,6 @@
 Hallo fellow,
-
-dir wurde gerade ein Account f&uuml; den <a href="http://der-naschmarkt.at">Naschmarkt</a> erstellt.
+<br><br>
+dir wurde gerade ein Account f&uuml;r den <a href="http://der-naschmarkt.at">Naschmarkt</a> erstellt.<br><br>
 Unter dem folgenden Link kannst du ein Konto aktivieren:
-
+<br><br>
 <a href="{{ url('/activate') }}?token={{ $token }}&id={{ $id }}">{{ url('/activate') }}?token={{ $token }}&id={{ $id }}</a>
